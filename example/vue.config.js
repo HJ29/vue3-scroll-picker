21ec0181321ebf05e8bd51a72ce83cc1f6f624a0
HongJian <hj@newtons.tech>
2020-09-28 00:37:05 +0800

    build docs

--- example/vue.config.js
+++ example/vue.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  outputDir: path.resolve(__dirname, '../docs'),
   chainWebpack: (config) => {
     config.resolve.alias.set('vue$', path.join(__dirname, 'node_modules/vue'));
   },
