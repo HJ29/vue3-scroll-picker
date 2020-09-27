3d54cce09cd71b32247bfad1bfb155daeb0b3c39
HongJian <hj@newtons.tech>
2020-09-28 00:51:53 +0800

    fixed path

--- example/vue.config.js
+++ example/vue.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   outputDir: path.resolve(__dirname, '../docs'),
+  publicPath: '/vue3-scroll-picker/',
   configureWebpack: {
     resolve: {
       symlinks: false,
