2d88d70cf175b83156da3870044d3946e0aeecf3
HongJian <hj@newtons.tech>
2020-09-28 00:49:30 +0800

    fixed path

--- example/vue.config.js
+++ example/vue.config.js
@@ -2,6 +2,11 @@ const path = require('path');
 
 module.exports = {
   outputDir: path.resolve(__dirname, '../docs'),
+  configureWebpack: {
+    resolve: {
+      symlinks: false,
+    },
+  },
   chainWebpack: (config) => {
     config.resolve.alias.set('vue$', path.join(__dirname, 'node_modules/vue'));
   },
